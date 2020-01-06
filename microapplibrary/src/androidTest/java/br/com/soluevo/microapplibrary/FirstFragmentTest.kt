3fdb1a26e8c95a1c981f16718978d2208093f300
angelorobsonmelo <angelorobsonmelo@gmail.com>
2020-01-06 17:38:03 -0300

    refactor test

--- microapplibrary/src/androidTest/java/br/com/soluevo/microapplibrary/FirstFragmentTest.kt
+++ microapplibrary/src/androidTest/java/br/com/soluevo/microapplibrary/FirstFragmentTest.kt
@@ -1,19 +1,13 @@
 package br.com.soluevo.microapplibrary
 
-import androidx.fragment.app.testing.FragmentScenario
-import androidx.fragment.app.testing.launchFragment
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
-import androidx.test.espresso.Espresso
-import androidx.test.espresso.Espresso.*
-import androidx.test.espresso.action.ViewActions
-import androidx.test.espresso.action.ViewActions.*
-import androidx.test.espresso.assertion.ViewAssertions
-import androidx.test.espresso.assertion.ViewAssertions.*
-import androidx.test.espresso.matcher.ViewMatchers
-import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mockito
@@ -23,17 +17,20 @@ import org.mockito.Mockito.verify
 @RunWith(AndroidJUnit4::class)
 class FirstFragmentTest {
 
-    @Test
-    fun testNavitationScreen() {
-        val mockNavController = Mockito.mock(NavController::class.java)
+    private val mockNavController = Mockito.mock(NavController::class.java)
+
+    @Before
+    fun setUp() {
         val scenario = launchFragmentInContainer<FirstFragment>(themeResId = R.style.AppTheme)
 
         scenario.onFragment {
             Navigation.setViewNavController(it.requireView(), mockNavController)
         }
+    }
 
+    @Test
+    fun testNavigationToInSecondFragment() {
         onView(withId(R.id.button)).perform(click())
-
         verify(mockNavController).navigate(R.id.action_firstFragment_to_secondFragment)
     }
 
