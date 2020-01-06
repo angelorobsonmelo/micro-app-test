0a46174940f74d905f58b9ed10a5030685624852
angelorobsonmelo <angelorobsonmelo@gmail.com>
2020-01-06 17:48:45 -0300

    refactor test

--- microapplibrary/src/androidTest/java/br/com/soluevo/microapplibrary/FirstFragmentTest.kt
+++ microapplibrary/src/androidTest/java/br/com/soluevo/microapplibrary/FirstFragmentTest.kt
@@ -10,14 +10,14 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.mockito.Mockito
+import org.mockito.Mockito.mock
 import org.mockito.Mockito.verify
 
 
 @RunWith(AndroidJUnit4::class)
 class FirstFragmentTest {
 
-    private val mockNavController = Mockito.mock(NavController::class.java)
+    private val mockNavController = mock(NavController::class.java)
 
     @Before
     fun setUp() {
